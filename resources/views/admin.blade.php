eadea2bedb74a9632a287ff949d842834631c5d5
Inkashi <zabolotin.nikitka@mail.ru>
2024-05-01 14:57:21 +0500

    стили для логин, регистрация, админка

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,82 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
+    @include('templates.include')
 </head>
+
 <body>
-	<div class="main">
-		<div>
-			<h5>Добавление учебного подразделения</h5>
-			<form action="createInstitute" method='post'>
-				@csrf
-				<input type="text" name='name'>
-				<button>+</button>
-			</form>
-		</div>
-		<div>
-			<h5>Добавить образовательную программу</h5>
-			<form action="createDirection" method='post'>
-				@csrf
-				<select id='select-state' name="institute" placeholder="подразделение">
-					<option value=""></option>
-					@foreach($institutes as $institute)
-				  		<option value="{{$institute->id}}">{{$institute->name}}</option>
-				  	@endforeach	
-				</select>
-				<input type="text" name='name' placeholder="наименование">
-				<input type="text" name='code' placeholder="код направления">
-				<button>+</button>
-			</form>
-		</div>
-		<div>
-			<h5>Удалить образовательную программу</h5>
-			<form action="deleteDirection" method='post'>
-				@csrf
-				<select id='select-state' name="direction" placeholder="наименование">
-					<option value=""></option>
-					@foreach($directiones as $direction)
-				  		<option value="{{$direction->id}}">{{$direction->name}}</option>
-				  	@endforeach	
-				</select>
-				<button>-</button>
-			</form>
-		</div>
-		<div>
-			<h5>Добавить руководителя ОПОП</h5>
-			<form action="createOPOP" method='post'>
-				@csrf
-				<select id='select-state' name="institute" placeholder="подразделение">
-					<option value=""></option>
-					@foreach($institutes as $institute)
-				  		<option value="{{$institute->id}}">{{$institute->name}}</option>
-				  	@endforeach	
-				</select>		
-				<select id='select-state' name="direction" placeholder="программа">
-					<option value=""></option>
-					@foreach($directiones as $direction)
-				  		<option value="{{$direction->id}}">{{$direction->name}}</option>
-				  	@endforeach	
-				</select>
-				<select id='select-state' name="user" placeholder="Пользователь">
-					<option value=""></option>
-					@foreach($users as $user)
-				  		<option value="{{$user->id}}">{{$user->second_name . ' ' . $user->first_name . ' ' . $user->third_name}}</option>
-				  	@endforeach	
-				</select>	
-				<button>+</button>
-			</form>
-		</div>
-	</div>
-	
+    <header>
+        <h1>Администратор учебных частей</h1>
+    </header>
+    <main class="main_admin">
+        <div class="container_admin">
+            <h2>Добавление учебного подразделения</h2>
+            <form action="createInstitute" method='post' class="form_admin">
+                @csrf
+                <input type="text" name='name' placeholder="наименование">
+                <button>+</button>
+            </form>
+        </div>
+        <hr>
+        <div class="container_admin">
+            <h2>Добавить образовательную программу</h2>
+            <form action="createDirection" method='post' class="form_admin">
+                @csrf
+                <select id='select-state' name="institute" placeholder="подразделение">
+                    <option value=""></option>
+                    @foreach ($institutes as $institute)
+                        <option value="{{ $institute->id }}">{{ $institute->name }}</option>
+                    @endforeach
+                </select>
+                <input type="text" name='name' placeholder="наименование">
+                <input type="text" name='code' placeholder="код направления">
+                <button>+</button>
+            </form>
+        </div>
+        <hr>
+        <div class="container_admin">
+            <h2>Удалить образовательную программу</h2>
+            <form action="deleteDirection" method='post' class="form_admin">
+                @csrf
+                <select id='select-state' name="direction" placeholder="наименование">
+                    <option value=""></option>
+                    @foreach ($directiones as $direction)
+                        <option value="{{ $direction->id }}">{{ $direction->name }}</option>
+                    @endforeach
+                </select>
+                <button>-</button>
+            </form>
+        </div>
+        <hr>
+        <div class="container_admin">
+            <h2>Добавить руководителя ОПОП</h2>
+            <form action="createOPOP" method='post' class="form_admin">
+                @csrf
+                <select id='select-state' name="institute" placeholder="подразделение">
+                    <option value=""></option>
+                    @foreach ($institutes as $institute)
+                        <option value="{{ $institute->id }}">{{ $institute->name }}</option>
+                    @endforeach
+                </select>
+                <select id='select-state' name="direction" placeholder="программа">
+                    <option value=""></option>
+                    @foreach ($directiones as $direction)
+                        <option value="{{ $direction->id }}">{{ $direction->name }}</option>
+                    @endforeach
+                </select>
+                <select id='select-state' name="user" placeholder="Пользователь">
+                    <option value=""></option>
+                    @foreach ($users as $user)
+                        <option value="{{ $user->id }}">
+                            {{ $user->second_name . ' ' . $user->first_name . ' ' . $user->third_name }}</option>
+                    @endforeach
+                </select>
+                <button>+</button>
+            </form>
+        </div>
+        <hr>
+    </main>
+
 </body>
 <script>
- $(document).ready(function () {
-      $('select').selectize({
-          sortField: 'text'
-      });
-  });
+    $(document).ready(function() {
+        $('select').selectize({
+            sortField: 'text'
+        });
+    });
 </script>
 
 </html>
